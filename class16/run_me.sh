6a2be3635eddc428534b7d343bc80fe675da698c
Meha Thakur <mthakur@ucsd.edu>
2025-12-05 14:35:56 -0800

    Adding more classes

---
+++ class16/run_me.sh
@@ -0,0 +1,4 @@
+kallisto quant -i hg19.ensembl -o SRR2156849_quant SRR2156849_1.fastq SRR2156849_2.fastq &
+kallisto quant -i hg19.ensembl -o SRR2156850_quant SRR2156850_1.fastq SRR2156850_2.fastq &
+kallisto quant -i hg19.ensembl -o SRR2156851_quant SRR2156851_1.fastq SRR2156851_2.fastq &
+kallisto quant -i hg19.ensembl -o SRR2156852_quant SRR2156852_1.fastq SRR2156852_2.fastq
